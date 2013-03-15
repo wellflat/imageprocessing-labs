11f36682c1226567624035a50a8ccc1ae046ef6c
wellflat <wellflat@gmail.com>
2013-03-15 12:59:27 +0900

    modified fft.js, supports TypedArray

--- cv/fft/fft.js
+++ cv/fft/fft.js
@@ -13,8 +13,8 @@
   }
 
   var version = {
-    release: '0.2.0',
-    date: '2012-01'
+    release: '0.3.0',
+    date: '2013-03'
   };
   FFT.toString = function() {
     return "version " + version.release + ", released " + version.date;
@@ -28,8 +28,8 @@
     init : function(n) {
       if(n !== 0 && (n & (n - 1)) === 0) {
         _n = n;
-        core._setVariables();
-        core._makeBitReversal();
+        core._initArray();
+        core._makeBitReversalTable();
         core._makeCosSinTable();
       } else {
         throw new Error("init: radix-2 required");
@@ -150,23 +150,25 @@
         }
       }
     },
-    // set variables
-    _setVariables : function() {
-      _bitrev = [];
-      _cstb = [];
-      // if(typeof Uint8Array !== 'undefined') {
-      //   _bitrev = new Uint8Array(_n);
-      // } else {
-      //   _bitrev = new Array(_n);
-      // }
-      // if(typeof Float64Array !== 'undefined') {
-      //   _cstb = new Float64Array(_n*1.25);
-      // } else {
-      //   _cstb = new Array(_n*1.25);
-      // }
+    // initialize the array (supports TypedArray)
+    _initArray : function() {
+      if(typeof Uint8Array !== 'undefined') {
+        _bitrev = new Uint8Array(_n);
+      } else {
+        _bitrev = [];
+      }
+      if(typeof Float64Array !== 'undefined') {
+        _cstb = new Float64Array(_n*1.25);
+      } else {
+        _cstb = [];
+      }
     },
-    // make bit reversal table
-    _makeBitReversal : function() {
+    // zero padding
+    _paddingZero : function() {
+      // TODO
+    },
+    // makes bit reversal table
+    _makeBitReversalTable : function() {
       var i = 0,
           j = 0,
           k = 0;
@@ -181,7 +183,7 @@
         _bitrev[i] = j;
       }
     },
-    // make trigonometiric function table
+    // makes trigonometiric function table
     _makeCosSinTable : function() {
       var n2 = _n >> 1,
           n4 = _n >> 2,
@@ -219,4 +221,199 @@
   }
   FFT.fft = core.fft1d;
   FFT.ifft = core.ifft1d;
+}).call(this);
+
+/**
+ * Spatial Frequency Filtering
+ * High-pass/Low-pass/Band-pass Filter
+ * Windowing using hamming window
+ */
+(function() {
+  var FrequencyFilter;  // top-level namespace
+  var _root = this;     // reference to 'window' or 'global'
+
+  if(typeof exports !== 'undefined') {
+    FrequencyFilter = exports;   // for CommonJS
+  } else {
+    FrequencyFilter = _root.FrequencyFilter = {};
+  }
+
+  // core operations
+  var _n = 0;
+  var core = {
+    init : function(n) {
+      if(n !== 0 && (n & (n - 1)) === 0) {
+        _n = n;
+      } else {
+        throw new Error("init: radix-2 required");
+      }
+    },
+    // swaps quadrant
+    swap : function(re, im) {
+      var xn, yn, i, j, k, l, tmp,
+          len = _n >> 1;
+      for(var y=0; y<len; y++) {
+        yn = y + len;
+        for(var x=0; x<len; x++) {
+          xn = x + len;
+          i = x + y*_n;
+          j = xn + yn*_n;
+          k = x + yn*_n;
+          l = xn + y*_n;
+          tmp = re[i];
+          re[i] = re[j];
+          re[j] = tmp;
+          tmp = re[k];
+          re[k] = re[l];
+          re[l] = tmp;
+          tmp = im[i];
+          im[i] = im[j];
+          im[j] = tmp;
+          tmp = im[k];
+          im[k] = im[l];
+          im[l] = tmp;
+        }
+      }
+    },
+    // applies High-Pass Filter
+    HPF : function(re, im, radius) {
+      var i = 0,
+          p = 0,
+          r = 0.0,
+          n2 = _n >> 1,
+          sqrt = Math.sqrt;
+      for(var y=-n2; y<n2; y++) {
+        i = n2 + (y + n2)*_n;
+        for(var x=-n2; x<n2; x++) {
+          r = sqrt(x*x + y*y);
+          p = x + i;
+          if(r < radius) {
+            re[p] = im[p] = 0;
+          }
+        }
+      }
+    },
+    // applies Low-Pass Filter
+    LPF : function(re, im, radius) {
+      var i = 0,
+          p = 0,
+          r = 0.0,
+          n2 = _n >> 1,
+          sqrt = Math.sqrt;
+      for(var y=-n2; y<n2; y++) {
+        i = n2 + (y + n2)*_n;
+        for(var x=-n2; x<n2; x++) {
+          r = sqrt(x*x + y*y);
+          p = x + i;
+          if(r > radius) {
+            re[p] = im[p] = 0;
+          }
+        }
+      }
+    },
+    // applies Band-Pass Filter
+    BPF : function(re, im, radius, bandwidth) {
+      var i = 0,
+          p = 0,
+          r = 0.0,
+          n2 = _n >> 1,
+          sqrt = Math.sqrt;
+      for(var y=-n2; y<n2; y++) {
+        i = n2 + (y + n2)*_n;
+        for(var x=-n2; x<n2; x++) {
+          r = sqrt(x*x + y*y);
+          p = x + i;
+          if(r < radius || r > (radius + bandwidth)) {
+            re[p] = im[p] = 0;
+          }
+        }
+      }
+    },
+    // windowing using hamming window
+    windowing : function(data, inv) {
+      var len = data.length,
+          pi = Math.PI,
+          cos = Math.cos;
+      for(var i=0; i<len; i++) {
+        if(inv === 1) {
+          data[i] *= 0.54 - 0.46*cos(2*pi*i/(len - 1));
+        } else {
+          data[i] /= 0.54 - 0.46*cos(2*pi*i/(len - 1));
+        }
+      }
+    }
+  };
+  // aliases (public APIs)
+  var apis = ['init', 'swap', 'HPF', 'LPF', 'BPF', 'windowing'];
+  for(var i=0; i<apis.length; i++) {
+    FrequencyFilter[apis[i]] = core[apis[i]];
+  }
+}).call(this);
+
+/**
+ * FFT Power Spectrum Viewer
+ */
+(function() {
+  var SpectrumViewer;  // top-level namespace
+  var _root = this;    // reference to 'window' or 'global'
+
+  if(typeof exports !== 'undefined') {
+    SpectrumViewer = exports;   // for CommonJS
+  } else {
+    SpectrumViewer = _root.SpectrumViewer = {};
+  }
+
+  // core operations
+  var _context = null,
+      _n = 0,
+      _img = null,
+      _data = null;
+  var core = {
+    init : function(context) {
+      _context = context;
+      _n = context.canvas.width,
+      _img = context.getImageData(0, 0, _n, _n);
+      _data = _img.data;
+    },
+    // renders FFT power spectrum on the canvas
+    render : function(re, im, islog) {
+      var val = 0,
+          i = 0,
+          p = 0,
+          spectrum = [],
+          max = 1.0,
+          imax = 0.0,
+          n2 = _n*_n,
+          log = Math.log,
+          sqrt = Math.sqrt;
+      for(var i=0; i<n2; i++) {
+        if(islog){
+          spectrum[i] = log(Math.sqrt(re[i]*re[i] + im[i]*im[i]));
+        } else {
+          spectrum[i] = sqrt(re[i]*re[i] + im[i]*im[i]);
+        }
+        if(spectrum[i] > max) {
+          max = spectrum[i];
+        }
+      }
+      imax = 1/max;
+      for(var j=0; j<n2; j++) {
+        spectrum[j] = spectrum[j]*255*imax;
+      }
+      for(var y=0; y<_n; y++) {
+        i = y*_n;
+        for(var x=0; x<_n; x++) {
+          val = spectrum[i + x];
+          p = (i << 2) + (x << 2);
+          _data[p] = 0;
+          _data[p + 1] = val;
+          _data[p + 2] = val >> 1;
+        }
+      }
+      _context.putImageData(_img, 0, 0);
+    }
+  };
+  // aliases (public APIs)
+  SpectrumViewer.init = core.init;
+  SpectrumViewer.render = core.render;
 }).call(this);
