6380673a0c41cabf96c8cff62123780520e082fe
Narazaka <info@narazaka.net>
2017-08-20 10:16:49 +0900

    remove not main files

--- cv/line-segment-detector/gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-﻿import gulp from 'gulp';
-import babel from 'gulp-babel';
-
-gulp.task('default', () => {
-    return gulp.src(['src/*.js', 'src/**/*.js'])
-        .pipe(babel())
-        .pipe(gulp.dest('dst'));
-});
