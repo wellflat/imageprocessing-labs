eca3960b1890a3b238e558b9590cea6d873772b6
ryohei tanaka <wellflat@gmail.com>
2019-07-22 17:42:08 +0900

    modified babel and eslint settings

--- ml/t-sne/src/tsne.js
+++ ml/t-sne/src/tsne.js
@@ -5,22 +5,30 @@
  */
 export default class tSNE {
     /**
-     * @param {number} perplexity
      * @param {number} dim
-     * @param {number} eps 
+     * @param {number} perplexity
+     * @param {number} eta
+     * @param {number} max_iter
      */
-    constructor(dim = 2, perplexity = 30, learning_rate = 100, max_iter = 1000) {
+    constructor(dim = 2, perplexity = 30, eta = 100, max_iter = 1000) {
         /** @type {number} */
         this.dim = dim;
         /** @type {number} */
         this.perplexity = perplexity;
         /** @type {number} */
-        this.learning_rate = learning_rate;
+        this.eta = eta;
         /** @type {number} */
         this.max_iter = max_iter;
     }
 
+    /**
+     * @param {number[][]|Float64Array[]} X 
+     */
     fit(X) {
+
+    }
+
+    #init() {
 
     }
 }
