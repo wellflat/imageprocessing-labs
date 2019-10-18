92fae996999acb79a3f9b6e074178c3cb98b310c
ryohei tanaka <wellflat@gmail.com>
2019-10-18 16:52:36 +0900

    added random data generater

--- ml/t-sne/src/index.js
+++ ml/t-sne/src/index.js
@@ -2,3 +2,4 @@ import tSNE from './tsne';
 
 const tsne = new tSNE();
 console.log(tsne);
+console.log(tsne.generateRandom(10, 0, 0.001));
--- ml/t-sne/src/tsne.js
+++ ml/t-sne/src/tsne.js
@@ -6,20 +6,17 @@
 export default class tSNE {
 
     /**
-     * @param {number} dim
      * @param {number} perplexity
      * @param {number} eta
-     * @param {number} max_iter
+     * @param {number} maxIter
      */
-    constructor(dim = 2, perplexity = 30, eta = 100, max_iter = 1000) {
-        /** @type {number} */
-        this.dim = dim;
+    constructor(perplexity = 30, eta = 100, maxIter = 1000) {
         /** @type {number} */
         this.perplexity = perplexity;
         /** @type {number} */
         this.eta = eta;
         /** @type {number} */
-        this.max_iter = max_iter;
+        this.maxIter = maxIter;
 
         this.init();
     }
@@ -27,10 +24,34 @@ export default class tSNE {
     /**
      * @param {number[][]|Float64Array[]} X 
      */
-    fit(X) {
+    compute(X) {
     }
 
     init() {
 
+    }
+
+    /**
+     * generate normally distributed random number array using Marsaglia polar method
+     * @param {number} n 
+     * @return {Float64Array}
+     */
+    generateRandom(n, mu, std) {
+        if (n % 2 != 0) {
+            throw new Error("n must be even number");
+        }
+        const data = new Float64Array(n);
+        for(let i = 0; i < n; i+=2) {
+            let u, v, r;
+            do {
+                u = 2 * Math.random() - 1;
+                v = 2 * Math.random() - 1;
+                r = u * u + v * v;
+            } while (r === 0 || r >= 1);
+            const mul = Math.sqrt(-2 * Math.log(r) / r);
+            data[i] = mu + (mul * u) * std;
+            data[i + 1] = mu + (mul * v) * std;
+        }
+        return data;
     }
 }
