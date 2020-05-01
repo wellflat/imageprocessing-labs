beda2cd18d75aa3ae46f2035852cacb8508f3f4d
wellflat <wellflat@gmail.com>
2020-05-01 17:23:41 +0900

    decomposed function

--- ml/t-sne/src/tsne.js
+++ ml/t-sne/src/tsne.js
@@ -16,6 +16,7 @@ export default class tSNE {
         this.data = data;
         /** @type {{perplexity:number, eta:number, alpha:number}} */
         this.params = params;
+        this.dims = 2; // mapping output dimention
         this.init();
     }
 
@@ -26,8 +27,7 @@ export default class tSNE {
             this.data = typed;
         }
         // initial solution
-        this.Y = generateRandom(this.data.length, 0, 0.001)
-
+        this.Y = new Float64Array(this.data.length*this.dims);
     }
 
     /**
@@ -38,12 +38,11 @@ export default class tSNE {
     compute(iterations) {
         // not yet impl.
         //
-        let Y = new Float64Array(this.data.length);
         const D = this.calculatePairwiseDistance(this.data);
-        console.log(D);
+        //console.log(D);
         //this.setP(D, this.params.perplexity);
         return new Promise((resolve, reject) => {
-            resolve(Y);
+            resolve(this.Y);
         });
     }
 
@@ -51,26 +50,30 @@ export default class tSNE {
      * set p_(ij) = p_(j|i) + p_(i|j) / 2n
      * @param {Float64Array[]} D pairwise distances
      */
-    setP(D, perplexity) {
+    setP(D) {
         const N = this.data.length;
+        const perpTarget = Math.log(this.params.perplexity);
         const P = new Float64Array(N * N);
         const condP = new Float64Array(N * N);
+        const tol = 1e-3, maxIter = 50;
 
         // compute pairwise affinities p_(j|i) with perplexity
         for (let i = 0; i < N; i++) {
-            // binary search to find a sigma2
-            let lb = 0.0, ub = 1e+10;//Number.POSITIVE_INFINITY;
-            let sigma2 = 1.0;
-            const MAX_ATTEMPTS = 50;
-            for (let bi = 0; bi < MAX_ATTEMPTS; bi++) {
-                let sigma2 = (lb + ub)/2.0;
+            // todo: decompose into functions
+            // binary search to find sigma
+            let lb = 0.0;
+            let ub = Number.POSITIVE_INFINITY;
+            let sigma = 1.0;
+            let searching = true, n = 0;
+            while(searching) {
                 for (let j = 0; j < N; j++) {
-                    if (i == j) {
+                    if (i === j) {
                         condP[j * N + i] = 0.0;
                     } else {
-                        condP[j * N + i] = Math.exp(-D[i * N + j] / (2.0 * sigma2));
+                        condP[j * N + i] = Math.exp(-D[i * N + j] / (2.0 * sigma));
                     }
                 }
+
                 let sum = 0.0;
                 for (let k = 0; k < N; k++) {
                     if (i != k) {
@@ -80,23 +83,35 @@ export default class tSNE {
                 for (let j = 0; j < N; j++) {
                     condP[j * N + i] /= sum;
                 }
+
                 // compute perplexity
-                let Hpi = 0.0;
+                let Hpi = 0.0;  // H(Pi) = -Σp_(j|i)log(p_(j|i))
                 for (let j = 0; j < N; j++) {
                     if (i != j) {
                         Hpi -= condP[j * N + i] * Math.log(condP[j * N + i]);
                     }
                 }
-                let perpPi = Math.pow(2.0, Hpi);
-                if (perpPi < perplexity) {
-                    lb = sigma2;
+                let perpPi = Math.pow(2.0, Hpi);  // Perp(Pi) = 2^H(Pi)
+
+                if (perpPi < perpTarget) {
+                    ub = sigma;
+                    if(ub === Number.POSITIVE_INFINITY) {
+                        sigma = sigma * 2;
+                    } else {
+                        sigma = (sigma + ub) / 2;
+                    }
                 } else {
-                    ub = sigma2;
+                    lb = sigma;
+                    if(lb === Number.NEGATIVE_INFINITY) {
+                        sigma = sigma / 2;
+                    } else {
+                        sigma = (sigma + lb) / 2;
+                    }
                 }
                 console.log(`sigma^2 ${i}: ${lb}`);
+
+                n++;
             }
-
-
         } // end loop of i
 
         // set p_(ij)
@@ -122,6 +137,11 @@ export default class tSNE {
             distance += (x1[i] - x2[i]) * (x1[i] - x2[i]);
         }
         return distance;
+       /*
+        return x1.reduce((sum, x1_i, i) => {
+            sum + (x1_i - x2[i]) * (x1_i - x2[i]), 0
+        });
+       */
     }
 
     /**
@@ -143,23 +163,69 @@ export default class tSNE {
     }
 
     /**
-     * calculate perplexity
+     * calculate entropy: H(Pi) = Σ-p_(j|i)log(p_(j|i))
      * @param {Float64Array} probs
      * @return {number}
      */
-    calculatePerplexity(probs) {
+    calculateEntropy(probs) {
         return probs.reduce(
             (sum, p) => sum - (p > 1e-7 ? p * Math.log(p) : 0), 0
         );
     }
 
     /**
-     * calcualte Kullback-Leibler Divergence (KLD)
-     * @param {number} p 
-     * @param {number} q
-     * @return {number}
+     * calculate gaussian probability: p_(i|j)
+     * @param {Float64Array} distances
+     * @param {number} sigma
+     * @return {number[]}
      */
-    calculateKLD(p, q) {
-        return p * Math.log(p / q);
+    calculateProbs(distances, sigma) {
+        let sum = 0.0;
+        const pij = distances.map(d => {
+            const prob = Math.exp(-d / (2.0 * sigma));
+            const currProb = d !== 0 ? prob : 0;  // p_(i|i) = 0
+            sum += currProb;
+            return currProb;
+        }).map(p => p/sum);
+        return pij;
+    }
+
+    /**
+     * perform binary search to find sigma
+     * @param {Float64Array[]} distances pairwise distances
+     * @param {number} i index of distance[i]
+     */
+    searchSigma(distances, i) {
+        const tol = 1e-3;
+        const maxIter = 50;
+        let lb = 0.0, ub = Number.POSITIVE_INFINITY;
+        let sigma = 1.0;
+        let isSearching = true;
+        let probs, entropy;
+        let n = 0;
+        while(isSearching) {
+            probs = this.calculateProbs()
+            n++;
+            //todo
+        }
+    }
+
+    /**
+     * calculate cost and gradient using Kullback-Leibler Divergence (KLD)
+     * @param {} Y
+     * @param {Float64Array[]} P
+     * @param {Float64Array[]} Q
+     */
+    costGrad(Y, P, Q) {
+        // todo
+        const dims = this.dims;
+        let grad = new Float64Array(dims);
+        let pij, qij, cost = 0.0;
+        for (let i = 0; i<dims; i++) {
+            if(i == j) break;
+            pij = P[i*N + j];
+            qij = Q[i*N + j];
+            cost += pij * Math.log(pij/qij); // calculate KLD
+        }
     }
 }
